ff2837a38645a0f6e99f0ab28f1605c591ccf0e1
Gabriela Soares <soaresgabriela1010@gmail.com>
2025-06-22 21:54:52 +0100

    Implemented ViewBinding in MainActivity.kt

--- app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
@@ -1,20 +1,26 @@
 package co.uk.bbk.culinarycompanion
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.viewModels
+import co.uk.bbk.culinarycompanion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val adapter = RecipeAdapter()
+
+        val dao = RecipesDatabase.getInstance(applicationContext).recipesDao()
+        viewModel.recipesDao = dao
+        viewModel.readAllRecipes()
+        viewModel.recipes.observe(this) { recipes ->
+            adapter.updateRecipes(recipes)
         }
     }
 }
