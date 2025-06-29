cf2f017bf788d14c57fb4c5a4b2f9b7e2494c6e4
Gabriela Soares <soaresgabriela1010@gmail.com>
2025-06-29 15:46:39 +0100

    Implemented setOnClickListener to the Add Recipe button to navigate the edit_recipe layout.

--- app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
@@ -1,8 +1,10 @@
 package co.uk.bbk.culinarycompanion
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import co.uk.bbk.culinarycompanion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,12 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = RecipeAdapter()
         binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        binding.buttonAddRecipe.setOnClickListener {
+            val intent = Intent(this, EditRecipeActivity::class.java)
+            startActivity(intent)
+        }
 
 
         val dao = RecipesDatabase.getInstance(applicationContext).recipesDao()
